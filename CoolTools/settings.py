9d8d5568da340093e286ded007074dd93319712b
Joshua Dunaway <joshuadunaway23@gmail.com>
2022-08-22 10:47:26 +0200

    Hiding SECRET_KEY using python-decouple

--- CoolTools/settings.py
+++ CoolTools/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9r!f@ddtdu+75!fvppoaspys*(zo12lw15jcswj*w@d1!8wdi5'
+# to protect key in public repo
+from decouple import config
+SECRET_KEY = config("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,7 +30,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     # my apps
     'blog.apps.BlogConfig',
